a81db278397cf622a7f5cb3c34ac4a60dbd999dd
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-07-03 12:33:34 +0530

    Add route for login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -30,3 +30,25 @@ router.post("/register", async (req, res) => {
     return res.status(500).json({ message: "Server Error" });
   }
 });
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      res.status(400).json({ message: "Invalid username or password!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      // isMatch is false
+      res.status(400).json({ message: "Invalid username or password!" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (e) {
+    res.status(500).json({ message: "Server error" });
+  }
+});

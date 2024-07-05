05e825083d6cc24de36da5730239ba50c25cb6e6
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-07-05 11:44:44 +0530

    Add add, update and delete routes

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -31,4 +31,54 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// Add todo
+router.post("/", auth, async (req, res) => {
+  const { text } = req.body;
+  try {
+    const newTodo = new Todo({
+      userId: req.user.id,
+      text: text,
+    });
+    const savedTodo = newTodo.save();
+    res.json(savedTodo);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// Update todo
+router.put("/:id", auth, async (req, res) => {
+  const { text, completed } = req.body;
+  try {
+    const todo = await Todo.findById(req.params.id);
+    console.log("Todo Found By Id", todo);
+    if (!todo) return res.status(404).json({ message: "Todo not found" });
+    if (todo.userId.toString() !== req.user.id)
+      return res.status(401).json({ message: "Not authorized!" });
+    todo.text = text ?? todo.text;
+    todo.completed = completed ?? todo.completed;
+    const updatedTodo = await todo.save();
+    res.json(updatedTodo);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// Delete a todo
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ msg: "Todo not found" });
+
+    if (todo.userId.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await Todo.findByIdAndDelete(todo.id);
+    res.json({ msg: "Todo deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;

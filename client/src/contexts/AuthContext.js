65378baefc9185dd4806d06c1adbb191c0add3de
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-07-05 17:58:46 +0530

    Add authcontext

---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("/api/auth/user", {
+          Authorization: `Bearer ${token}`,
+        })
+        .then((response) => setUser(response.data))
+        .catch(() => {
+          localStorage.removeItem("token");
+        });
+    }
+  }, []);
+
+  const login = async (username, password) => {
+    const response = await axios.post("/api/auth/login", {
+      username,
+      password,
+    });
+    localStorage.setItem("token", response.data.token);
+  };
+
+  const register = async (username, password) => {
+    const response = await axios.post("/api/auth/register", {
+      username,
+      password,
+    });
+    localStorage.setItem("token", response.data.token);
+    setUser(response.data.user);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };

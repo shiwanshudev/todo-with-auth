916f1606212f7003a1c351a22a6da2c3849c8b53
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-05 12:25:09 +0530

    Add username in token when login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -62,9 +62,13 @@ router.post("/login", async (req, res) => {
       // isMatch is false
       return res.status(400).json({ message: "Invalid username or password!" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
     return res.status(200).json({ token });
   } catch (e) {
     return res.status(500).json({ message: "Server error" });
